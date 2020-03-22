79e394187bdf22db21850de221a101572381ff3f
Note45 <antonion.2048@gmail.com>
2020-03-22 14:22:44 -0300

    Created function convertToDecimal and renderConvert

--- bin2dev/src/components/Converter/index.js
+++ bin2dev/src/components/Converter/index.js
@@ -3,13 +3,25 @@ import React from 'react';
 import './styles.css';
 
 function Converter() {
+  function convertToDecimal() {
+    let binaryValue = document.getElementsByClassName('binaryInput')[0].value
+    return parseInt(binaryValue, 2)
+  }
+
+  function renderConvert() {
+    let decimalOut = convertToDecimal()
+
+    let textElementOut = document.getElementsByClassName('decimalOut')[0]
+    textElementOut.innerHTML = decimalOut
+  }
+
   return(
     <div className='converterContainer'>
       <p className='binaryText'>Binary:</p>
-      <input type='text' className='binaryInput'></input>
+      <input type='number' className='binaryInput'></input>
       <p className='decimalText'>Decimal:</p>
       <p className='decimalOut'></p>
-      <button className='bntConvert'>Convert</button>
+      <button className='bntConvert' onClick={renderConvert}>Convert</button>
     </div>
   );
 }
